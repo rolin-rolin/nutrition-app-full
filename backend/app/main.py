c84c18cc9b4640237dd54dcbaa15773971809409
rolin-rolin <rolin71110@gmail.com>
2025-08-10 20:36:04 -0400

    sneaky lil slash pmo

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,7 +11,7 @@ app = FastAPI(
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://oa-recs.vercel.app/"],  # Frontend URLs
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://oa-recs.vercel.app"],  # Frontend URLs
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],  # Include OPTIONS
     allow_headers=["*"],
